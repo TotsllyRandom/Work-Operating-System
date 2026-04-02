ac02893e061c9f1151f7fb25776b4b41e62142ec
Totally_RandomYT <68128408+TotsllyRandom@users.noreply.github.com>
2026-04-02 10:40:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ pygame.init()
 
 # Displaying a window of height 
 # 500 and width 400
-pygame.display.set_mode((640, 360))
+screen = pygame.display.set_mode((640, 360))
 
 # Here we set name or title of our
 # pygame window
-pygame.display.set_caption('GeeksforGeeks')
+pygame.display.set_caption('WorkOS')
 
 # Here we load the image we want to 
 # use
-Icon = pygame.image.load('gfglogo.png')
+Icon = pygame.image.load('assets/icon.jpg')
 
 # We use set_icon to set new icon
 pygame.display.set_icon(Icon)
@@ -25,12 +25,16 @@ running = True
 
 # Keep game running till running is true
 while running:
-    
+    screen.fill((0, 0, 0))  # black background
     # Check for event if user has pushed 
     # any event in queue
+
     for event in pygame.event.get():
         
         # If event is of type quit then set 
         # running bool to false
         if event.type == pygame.QUIT:
             running = False
+    pygame.display.update()
+
+pygame.quit()
